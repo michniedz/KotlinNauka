553e9429dd4da06fff2e61ccaa82e49126212d4a
michniedz <michniedz@gmail.com>
2024-02-08 23:01:52 +0100

    Zad01

---
+++ Lekcja01/src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val imie = "Tekst"
+    print("Podaj imię: ")
+    val name = readlnOrNull().toString()
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    println("Hello, $imie!")
+    println("Hello, $imie!")
+    print("Hello, $name!")
+    // pojedyncz linia komentarza
+    /*
+    * komentarze wielolinijkowe
+    * */
+}
