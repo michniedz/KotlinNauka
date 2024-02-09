e9c2fef15269efabe5a99d70cbfc05b1d524948c
michniedz <34127556+michniedz@users.noreply.github.com>
2024-02-09 14:49:05 +0100

    Lek04

---
+++ Lekcja04/src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+package org.example
+
+import kotlin.math.*
+
+fun main() {
+    //operatory logiczne
+    val a = 5
+    val b = 10
+    var x: Boolean
+    x = a == b
+    println(x)  // false
+    println(a != b) // true
+
+    println(1 > 2) // flase
+    println(1 < 2) // true
+    println(1 >= 2) // flase
+    println(1 <= 2) // true
+}
