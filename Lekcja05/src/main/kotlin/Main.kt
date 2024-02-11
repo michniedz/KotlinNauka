ffb22188ba20337b2f260682582efa063f63f7af
michniedz <michniedz@gmail.com>
2024-02-11 12:59:04 +0100

    Lek05

--- Lekcja05/src/main/kotlin/Main.kt
+++ Lekcja05/src/main/kotlin/Main.kt
@@ -7,5 +7,25 @@ fun main() {
     val a = 5
     val b = 10
     var x: Boolean
-
+    x = 5 > 5 || a < b // oprator "lub" "OR"  false OR true
+    println(x)  //true
+    x = 5 > 5 && a < b // oprator "lub" "OR" false AND true
+    println(x)  //false
+    println(false || false)  //false
+    println(false || true)  //true
+    println(true || false)  //true
+    println(true || true)  //true
+    println(false && false)  //false
+    println(false && true)  //false
+    println(true && false)  //false
+    println(true && true)  //true
+    // OPERATOR NOT ! - zaprzeczenie
+    println(!true)  //false
+    println(!false)  //true
+    // kolejność wykonywania działań
+    // AND - iloczyn logiczny
+    // OR - suma logiczna
+    println(true || false && false)  //true
+    println((true || false) && false)  //false
+    println(true || (false && false))  //true
 }
